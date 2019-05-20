1c9e6c9e54e8e061e3ffb0f42fa1bb1a6bf77cb1
David <dav-ss@mail.ru>
2019-05-20 11:48:54 -0700

    Adding questionbank,settting TextView

--- app/src/main/java/com/davidvardanyan/quizzler/MainActivity.java
+++ app/src/main/java/com/davidvardanyan/quizzler/MainActivity.java
@@ -5,12 +5,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
   Button mTrueButton;
   Button mFalseButton;
+  TextView mQuestionTextView;
+  int mIndex;
+   int mQuestion;
+
+
+  private TrueFalse[] mQuestionBank = new TrueFalse[]{
+
+          new TrueFalse(R.string.question_1,true),
+          new TrueFalse(R.string.question_2,true),
+          new TrueFalse(R.string.question_3,true),
+          new TrueFalse(R.string.question_4,true),
+          new TrueFalse(R.string.question_5,true),
+          new TrueFalse(R.string.question_6,false),
+          new TrueFalse(R.string.question_7,true),
+          new TrueFalse(R.string.question_8,false),
+          new TrueFalse(R.string.question_9,true),
+          new TrueFalse(R.string.question_10,true),
+          new TrueFalse(R.string.question_11,false),
+          new TrueFalse(R.string.question_12,false),
+          new TrueFalse(R.string.question_13,true),
+
+
+
+
+  };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +45,10 @@ public class MainActivity extends Activity {
 
      mTrueButton = findViewById(R.id.true_button);
      mFalseButton = findViewById(R.id.false_button);
+     mQuestionTextView = findViewById(R.id.question_text_view);
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
 
      mTrueButton.setOnClickListener(new View.OnClickListener() {
